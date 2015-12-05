9ce0fa42552f0f9c16dd3a6c604ffcf1bc3aa7d2
Bourquencier <laurie.bourquencier@gmail.com>
2015-12-05 16:05:00 +0100

    Algo Strategies

--- src/DefensiveStrategy.java
+++ src/DefensiveStrategy.java
@@ -1,37 +1,44 @@
+import java.util.Iterator;
+
 // EN COURS DE TEST ----
 // C'EST ICI QUE L'ON VA CODER LES ALGOS DANS CHAQUE METHODE
 public class DefensiveStrategy implements Strategy {
 	private String nomStrat;
-	/*
-	 * liste d'attributs nécessaire ? 
-	 */
 	
-	// Constructeur
 	public DefensiveStrategy(String strat) {
 		super();
 		this.nomStrat = strat;
 	}
 
 	public void jouerCarte(Manche m, Partie p) {
-		System.out.println("Stratégie jouée "/*par  " +*/  + " est : " + this.nomStrat);
-		
-		// CODE DE JOUER CARTE
+		System.out.println("Execution du joueurCarte de " + this.nomStrat + "Strategy après être passé dans executeStrategy");System.out.println("Execution du joueurCarte de " + this.nomStrat + "Strategy après être passé dans executeStrategy");
+
 		Carte c = this.choisirCarte(m, p);   // coder choisir carte
 		this.choisirAction(c, m, p);         // coder choisir action
+		// choisirAction(int) ??
 	}
 
 	public Carte choisirCarte(Manche m, Partie p) {
 		// code de choisirCarte pour le joueurVirtuel
-		return null;
+		System.out.println("\nAu tour de joueur virtuel " + "de jouer!\n");
+		System.out.println("Le joueur virtuel choisi sa carte...");
+		
+		// parcourir la main du joueur virtuel
+		// choisir la carte qui a le plus grand indice en fonction de la saison 
+		//		au niveau de l'action GEANT
+		// ensuite retourner la carte choisie 
+		
+		return this.choisirCarte(m, p);
 	}
 
 	public void choisirAction(Carte c, Manche m, Partie p) {
 		// code de choisirAction pour le joueurVirtuel
+		System.out.println("Le joueur virtuel choisi son action...");
 		
-	}
-
-	public String getNomStrat() {
-		return nomStrat;
-	}
-
+		// si le nombre de graines du joueur virtuel > 4
+		//	alors on le fait jouer engrais pour faire pousser des menhirs
+		// sinon on joue géant 
+		// sinon farfadet 
+		
+	}	
 }
